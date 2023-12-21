acdb4b4f96d72df5895e7f1b8e75422bc52536a1
BaoTruong101197 <BaoTruong101197@gmail.com>
2023-12-21 22:10:11 +0700

    Fix opacity mask

--- build.sh
+++ build.sh
@@ -11,21 +11,21 @@ cd ../../Apps/UICommon
 rm -rf build
 mkdir build
 cd build
-~/Qt/6.5.1/gcc_64/bin/qmake6 ..
+~/Qt/6.5.3/gcc_64/bin/qmake6 ..
 make
 
 cd ../../../MiddlewareLayer
 rm -rf build
 mkdir build
 cd build
-~/Qt/6.5.1/gcc_64/bin/qmake6 ..
+~/Qt/6.5.3/gcc_64/bin/qmake6 ..
 make
 
 cd ../../Apps/Phone
 rm -rf build
 mkdir build
 cd build
-~/Qt/6.5.1/gcc_64/bin/qmake6 ..
+~/Qt/6.5.3/gcc_64/bin/qmake6 ..
 make
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/projects/IPhoneOS/Executable/
 export LD_LIBRARY_PATH
