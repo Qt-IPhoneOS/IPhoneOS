9693ca84f3b2aa0b455ecc14c311c1d45ed41317
BaoTruong101197 <BaoTruong101197@gmail.com>
2024-01-08 01:32:18 +0700

    Fix bug

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 version=$(ls ~/Qt/ | grep "[0-9]")
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/projects/IPhoneOS/build/executable/
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${PWD}/build/executable/
 export LD_LIBRARY_PATH
 QT_PLUGIN_PATH=$HOME/Qt/${version}/gcc_64/plugins${QT_PLUGIN_PATH:+:$QT_PLUGIN_PATH}
 export QT_PLUGIN_PATH
